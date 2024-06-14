636a2e6ad90296896cb8af6d7f2aa5e89bea243d
Jijun Leng <jijunleng@gmail.com>
2024-06-13 20:37:05 -0700

    fix: fix the tailwind config

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -3,9 +3,9 @@ import type { Config } from 'tailwindcss';
 const config: Config = {
   darkMode: 'media',
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
