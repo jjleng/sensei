b3dcd831a2053b57f0e17ba9875b95e7940db9dc
Jijun Leng <jijunleng@gmail.com>
2024-06-26 12:18:04 -0700

    feat: slide down animation for images

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -80,10 +80,23 @@ const config = {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' },
         },
+        slideDown: {
+          '0%': { transform: 'translateY(-100%)' },
+          '100%': { transform: 'translateY(0)' },
+        },
+        appear: {
+          '0%': {
+            opacity: '0',
+          },
+          '100%': {
+            opacity: '1',
+          },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'slide-down': 'slideDown 0.5s ease-out',
       },
     },
   },
