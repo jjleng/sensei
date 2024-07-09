3bb56d0ca986bc405dd0f7111f15cd55d15fbaf5
Jijun Leng <jijunleng@gmail.com>
2024-07-09 13:31:06 -0700

    feat: default to searxng for dev

--- backend/sensei_search/tools/search/__init__.py
+++ backend/sensei_search/tools/search/__init__.py
@@ -1,3 +1,11 @@
 from .base import *
 from .bing import *
 from .searxng import *
+import os
+
+def get_search_tool() -> SearchTool:
+    env = os.getenv("ENV", "development")
+    if env == "development":
+        return SearxNG()
+    else:
+        return Bing()
